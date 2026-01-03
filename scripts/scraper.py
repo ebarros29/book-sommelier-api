749eab1f62a06b627eadc59295e02bd854b26da1
Victor Castro <victorcastro.dev@gmail.com>
2026-01-03 12:29:29 -0300

    feat: refina lógica de scraping e implementa persistência resiliente
    - Implementada extração detalhada de atributos: Título, Preço, Rating, Categoria e Imagem.
    - Adicionado tratamento de dados via Regex para conversão de moedas e normalização de preços (Money Pattern/Inteiros).
    - Implementado mapeamento de Ratings CSS para valores ordinais numéricos.
    - Refatorada a CSVWriter para utilizar Protocolo de Contexto (__enter__/__exit__) para maior performance de I/O.

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -1,6 +1,7 @@
 from writer import CSVWriter
 from storageInterface import DataStorage
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 import requests
 import re
 
@@ -45,9 +46,9 @@ class BookScraper():
     def get_all_books_urls(self):
         for i in range(1, self.last_page + 1):
             url = f"{self.base_url}page-{i}.html"
-            soap = self.get_soup(url)
+            soup = self.get_soup(url)
             
-            books = soap.find_all("article", class_="product_pod")
+            books = soup.find_all("article", class_="product_pod")
             
             for book in books:
                 tag_a = book.h3.a
@@ -55,23 +56,133 @@ class BookScraper():
                 relative_url = tag_a['href']
 
                 self._books_urls.append(relative_url)
+    
+    def _parse_price_string(self, price_raw: str):
+        """
+        Input: "£51.77"
+        Output: ("£", 5177)
+        """
         
+        currency_maps = {
+            "£": "GBP",
+            "€": "EUR",
+            "$": "USD",
+            "R$": "BRL"
+        }
+        
+        match = re.search(r"([^0-9.Â]+)\s*([\d.]+)", price_raw)
+        
+        if match:
+            currency_symbol = match.group(1).strip()
+            price_value = float(match.group(2))
+            
+            currency_code = currency_maps.get(currency_symbol, "UNKNOWN")
+            
+            price_in_cents = int(round(price_value * 100))
+            
+            return currency_code, price_in_cents
+        
+        return "N/A", 0
+    
+    def _find_book_title(self, soup: BeautifulSoup):
+        """
+        Obtem o título do livro
+        """
+        product_main = soup.find("div", class_="product_main")
+        return product_main.h1.text.strip()
+    
+    def _find_book_price(self, soup: BeautifulSoup):
+        """
+        Obtem o preço do livro
+        """
+        price = soup.find("p", class_="price_color")
+        return self._parse_price_string(price.text)
+    
+    def _find_book_rating(self, soup: BeautifulSoup):
+        """
+        Obtem a avaliação
+        """
+        rating_map = {
+            "One": 1,
+            "Two": 2,
+            "Three": 3,
+            "Four": 4,
+            "Five": 5
+        }
+        
+        tag_p = soup.find("p", class_="star-rating")
+        
+        if tag_p:
+            classes = tag_p.get("class", [])
+            
+            for cls in classes:
+                if cls in rating_map:
+                    return rating_map[cls]
+                
+    def _find_book_category(self, soup: BeautifulSoup):
+        """
+        Obtem a categoria
+        """
+        
+        breadcrumb = soup.find('ul', class_="breadcrumb")
+        items = breadcrumb.find_all('li')
+        
+        return items[2].get_text(strip=True)
+    
+    def _find_book_image(self, soup: BeautifulSoup):
+        """
+        Obtem a url da imagem
+        """
+        product_gallery = soup.find("div", id="product_gallery")
+    
+        if product_gallery:
+            img_tag = product_gallery.find("img")
+            
+            if img_tag and img_tag.get("src"):
+                relative_path = img_tag.get("src")
+                
+                full_image_url = urljoin(self.base_url, relative_path)
+                
+                return full_image_url
+            
+        return None
+
+    def save_books(self):
+        total = len(self._books_urls)
+        for i, book_url in enumerate(self._books_urls, 1):
+            soup = self.get_soup(urljoin(self.base_url, book_url))
+            
+            title = self._find_book_title(soup)
+            currency, price = self._find_book_price(soup)
+            rating = self._find_book_rating(soup)
+            category = self._find_book_category(soup)
+            img_url = self._find_book_image(soup)
+            
+            data = {
+                "title": title,
+                "price": price,
+                "currency": currency,
+                "rating": rating,
+                "category": category,
+                "img_url": img_url
+            }
+            
+            print(f"[{i}/{total}] Processando: {title}")
+            self.storage.save_item(data)
+            
     def run(self):
         """
         Fluxo principal de execução
         """
         self.set_last_page_number()
         self.get_all_books_urls()
-        
-        print(self._books_urls)
+        self.save_books()
         
 if __name__ == "__main__":
     filename = "./data/book_data"
-    fieldnames = ["title", "price", "rating", "category", "img_url"]
+    fieldnames = ["title", "price", "currency", "rating", "category", "img_url"]
         
-    scraper = BookScraper(CSVWriter(
-       filename,
-       fieldnames
-    ))
-    
-    scraper.run()
+    with CSVWriter("./data/book_data", fieldnames) as writer:
+        scraper = BookScraper(storage=writer)
+        writer.save_header()
+        scraper.run()
